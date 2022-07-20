1a6e8a0076f6ca775d454e430bd8e97e3cb59dcf
RojanGamingYT <71162720+RojanGamingYT@users.noreply.github.com>
2022-07-20 13:30:07 +0545

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ generateBtn.addEventListener("click", () => {
     if(!ssValue) return;
     preValue = ssValue;
     generateBtn.innerText = "Generating Screenshot...";
-    ssImg.src = `https://image.thum.io/get/fullpage/png/${ssValue}`;
+    ssImg.src = `http://image.thum.io/get/width/1200/wait/3/www.${ssValue}`;
     ssImg.addEventListener("load", () => {
         wrapper.classList.add("active");
         generateBtn.innerText = "Generate Screenshot";
